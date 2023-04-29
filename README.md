9dd4c45c0f0a2f18b45e9ab22c7cc909704cd3c2
bsimmonds28 <bsimmonds28@gmail.com>
2023-04-29 19:32:04 -0400

    draft 3

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-portfolio
+
+## resources
+https://www.freecodecamp.org/news/build-portfolio-website-react/
+https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/regular
+http://dotnetqueries.com/Article/162/how-to-display-pdf-in-bootstrap-modal-popup
+https://tailwindcss.com/docs/installation
+https://react.dev/learn
+University of Connecticut Coding Bootcamp
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -20,32 +20,17 @@ export default function Resume() {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Resume
-                  </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
-                </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <embed src="./bethResume.pdf" frameborder="0" width="100%" height="400px"/>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
                 </div>
               </div>
             </div>
