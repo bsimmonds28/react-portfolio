93425c7750fdf3392ddaf9bcd8ae06e437f36def
bsimmonds28 <bsimmonds28@gmail.com>
2023-04-29 17:07:46 -0400

    first draft

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+//import Header from './components/Header';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <head>
+            <link href="/dist/output.css" rel="stylesheet"></link>
+        </head>
+    )
+}
