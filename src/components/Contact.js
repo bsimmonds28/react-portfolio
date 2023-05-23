7b98e956e256b493154ba4c88eda33f910da1963
bsimmonds28 <bsimmonds28@gmail.com>
2023-05-23 14:13:40 -0400

    adjust form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/", {
+    fetch("https://formspree.io/bsimmonds28@gmail.com", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
