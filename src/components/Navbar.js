007544b0166c48519e128862628fa2ae73205c42
bsimmonds28 <bsimmonds28@gmail.com>
2023-05-23 13:21:29 -0400

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,22 @@ export default function Navbar() {
   return (
     <header className="bg-zinc-400 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Bethany Simmonds
-          </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-white	flex flex-wrap items-center text-base justify-center">
-          <a href="#about" className="mr-5 hover:text-white">
+        <button className="title-font font-medium text-white ml-3 text-xl mb-4 md:mb-0">
+          Bethany Simmonds
+        </button>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-white flex flex-wrap items-center text-base justify-center">
+          <button className="mr-5 hover:text-white" onClick={() => window.location.href = '#about'}>
             About Me
-          </a>
-          <a href="#projects" className="mr-5 hover:text-white">
+          </button>
+          <button className="mr-5 hover:text-white" onClick={() => window.location.href = '#projects'}>
             Projects
-          </a>
-          <a href="#resume" className="mr-5 hover:text-white">
+          </button>
+          <button className="mr-5 hover:text-white" onClick={() => window.location.href = '#resume'}>
             Resume
-          </a>
-          <a href="#contact" className="mr-5 hover:text-white">
+          </button>
+          <button className="mr-5 hover:text-white" onClick={() => window.location.href = '#contact'}>
             Contact
-          </a>
+          </button>
         </nav>
       </div>
     </header>
